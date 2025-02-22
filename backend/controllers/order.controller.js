e0f3e38df9d00f29cf290b7b96a68bd3fc492c25
Irungusam <irungusam8@gmail.com>
2025-02-22 19:29:00 +0300

    Add logic for placing order by COD payment method

--- backend/controllers/order.controller.js
+++ backend/controllers/order.controller.js
@@ -1,7 +1,35 @@
+import orderModel from "../models/order.model.js";
+import userModel from "../models/user.model.js";
 
 //Placing order using COD method
 const placeOrder = async (req,res) => {
     
+    try {
+        
+        const {userId,items,amount,address} = req.body;
+
+        const orderData = {
+            userId,
+            items,
+            address,
+            amount,
+            paymentMethod:"COD",
+            payment:false,
+            date: Date.now()
+        }
+
+        const newOrder = new orderModel(orderData)
+        await newOrder.save()
+
+        await userModel.findByIdAndUpdate(userId,{cartData:{}})
+
+        res.json({success:true, message:"Order Placed"})
+
+    } catch (error) {
+        console.log(error);
+        res.json({success:false, message:error.message})
+        
+    }
 }
 
 //Placing order using Stripe method
@@ -12,6 +40,15 @@ const placeOrderStripe = async (req,res) => {
 //Placing order using Razorpay method
 const placeOrderRazorpay = async (req,res) => {
     
+    try {
+        
+        const orders = await orderModel.find({})
+        res.json({success:true,orders})
+
+    } catch (error) {
+        console.log(error);
+        res.json({ success: false, message: error.message });
+    }
 }
 
 //All Orders data for Admin Panel
@@ -19,9 +56,19 @@ const allOrders = async (req,res) => {
     
 }
 
-//All Orders data for Admin Panel
+//User order data-Frontend
 const userOrders = async (req,res) => {
     
+    try {
+        
+        const {userId} = req.body;
+
+        const orders = await orderModel.find({userId})
+        res.json({success:true,orders})
+    } catch (error) {
+        console.log(error);
+        res.json({ success: false, message: error.message });
+    }
 }
 
 // Update order status - Admin
